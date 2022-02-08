f99931414ab6aaffedbbbb93a79c726d364897d1
colinfire57 <74149019+Aerialflame7125@users.noreply.github.com>
2022-02-08 20:09:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var SQUARE = 
+{
+  color:'yellow'
+}
+
+var T-BLOCK =
+{
+  color:'purple'
+}
+
+var LINE-BLOCK =
+{
+  color:'lightblue'
+}
+
+var S-BLOCK = 
+{
+  color:'green'
+}
+
+var Z-BLOCK = 
+{
+  color:'red'
+}
+
+var L-BLOCK = 
+{
+  color:'blue'
+}
+
+var REVL-BLOCK = 
+{
+  color:'orange'
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+    background(255);
+}
+
+function draw() {
+
+}
